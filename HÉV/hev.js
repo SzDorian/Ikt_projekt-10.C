ca1e1c0a8c031089a2a37459412d50c802ff8e21
Szabó Dorián <SzaboDo@KKSZKI.LOCAL>
2022-10-06 12:30:02 +0200

    Kész a menetidő számitás

---
+++ HÉV/hev.js
@@ -0,0 +1,10 @@
+function szamol()
+{
+var indulo=document.getElementById("select1").value;
+var érkezo=document.getElementById("select2").value;
+
+var eredmeny=indulo-érkezo;
+
+alert(Math.abs(eredmeny))
+
+}
