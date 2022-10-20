a1983f0281db8ea6a562836a5569a8958936451a
Szabó Dorián <SzaboDo@KKSZKI.LOCAL>
2022-10-20 11:54:58 +0200

    Az első 3 feladat kész

---
+++ python/gyakorlo.py
@@ -0,0 +1,52 @@
+#Első feladat
+'''
+list = []
+
+szam1 = int(input('Kérem az első számot:'))
+szam2 = int(input('Kérem az második számot:'))
+szam3 = int(input('Kérem a harmadik számot:'))
+
+list.append(szam1)
+list.append(szam2)
+list.append(szam3)
+
+def kisebb():
+    print(min(list))
+
+kisebb()    
+'''
+#Második feladat
+'''
+list = []
+
+szam1 = int(input('Kérem az első számot:'))
+szam2 = int(input('Kérem az második számot:'))
+szam3 = int(input('Kérem a harmadik számot:'))
+
+list.append(szam1)
+list.append(szam2)
+list.append(szam3)
+
+def nagyobb():
+    print(max(list))
+
+nagyobb()    
+'''
+
+#Harmadik feladat
+'''
+x = int(input('Kérek egy pontszámot:'))
+if x<50:
+    print('Elégtelen')
+elif 50<=x<60:
+    print('Elégséges')
+elif 60<=x<70:
+    print('Közepes')
+elif 70<=x<85:    
+    print('Jó')
+elif x>=85:
+    print('Jeles')           
+'''
+
+
+
